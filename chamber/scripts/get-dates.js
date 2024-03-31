76f517173b2e30640f56021137d2330b078875cc
OMoncada <132164886+OMoncada@users.noreply.github.com>
2024-03-31 02:27:32 -0300

    Add files via upload

---
+++ chamber/scripts/get-dates.js
@@ -0,0 +1,12 @@
+const copyrightYear = {
+    year: "numeric"
+};
+
+document.querySelector('footer #year').textContent = new Date().toLocaleDateString("en-US", copyrightYear);
+
+// References to show the date and time the document was last modified
+// https://www.tutorialspoint.com/How-to-show-the-date-and-time-the-document-was-last-modified-with-JavaScript
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/lastModified
+let lastModificationDate = document.lastModified;
+
+document.querySelector('#lastModified').innerHTML = `Last Modification: ${lastModificationDate}`;
