5e0fa53279ebc8b4a6cf100d35d620667ec60e27
OMoncada <132164886+OMoncada@users.noreply.github.com>
2024-03-31 01:31:10 -0300

    Delete chamber/scripts directory
    eliminar

--- chamber/scripts/hamburger.js
+++
@@ -1,7 +0,0 @@
-const hamButton = document.querySelector('#hamburger-menu');
-const navElement = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-    navElement.classList.toggle('open');
-    hamButton.classList.toggle('open');
-});
