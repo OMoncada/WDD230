d2468b87fdea0b26a8942b0dae91d99714ce644a
OMoncada <132164886+OMoncada@users.noreply.github.com>
2024-03-31 01:40:03 -0300

    Add files via upload

---
+++ chamber/scripts/hamburger.js
@@ -0,0 +1,7 @@
+const hamButton = document.querySelector('#hamburger-menu');
+const navElement = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
